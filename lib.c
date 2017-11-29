2a06ed764903dbc73cc373e16ef7fa0a5efbfd78
Segundo Fariña <segundofarina@me.com>
2017-11-29 20:43:21 -0300

    libreria

--- lib.c
+++ lib.c
@@ -3,29 +3,48 @@
 #include "lib.h"
 #include <unistd.h>
 #include <string.h>
+#include <regex.h>        
 
 
-char left [5][FRAME_WIDTH][FRAME_HEIGHT];
+action * actions;
+int actionsLen=0;
 
+int isVariable (char * string){
+	 int    status;
+    regex_t    re;
 
+    if (regcomp(&re, "[a-zA-Z]([a-zA-Z0-9_])*", REG_EXTENDED|REG_NOSUB) != 0) {
+        return(0);      /* Report error. */
+    }
+    status = regexec(&re, string, (size_t) 0, NULL, 0);
+    regfree(&re);
+    if (status != 0) {
+        return(0);      /* Report error. */
+    }
+    return(1);
+}
 
-
-void printMovement (char movement [5][FRAME_WIDTH][FRAME_HEIGHT], int position){
+void printMovement (char movement [ACTION_LENGTH][FRAME_WIDTH][FRAME_HEIGHT], int position, int direction){
 	int i,j,k,l;
+	int offset = FRAME_WIDTH*(SCREEN_SPACES - position);
+	if(position > SCREEN_SPACES){
+		printf("Inavlid postion\n");
+	}
+	
 
-	char spaces[120]={0};
-		for(k=0; k<position ;k++){
-			strcat(spaces,"          ");
-		}
+	char spaces[FRAME_WIDTH*SCREEN_SPACES+1]={0};
+	for(k=0; k<FRAME_WIDTH*SCREEN_SPACES ;k++){
+		spaces[k]=' ';
+	}
+	spaces[k]=0;
 
-for (l=1 ;l<3 ;l++){
+	
 
 	for (i=0; i<ACTION_LENGTH ;i++){
-		char offset[10]={0};
 		for(j=0; j<FRAME_HEIGHT; j++){
-			printf("%s",spaces+(i+l*ACTION_LENGTH) );
+			printf("%s", (spaces + (i*2)*direction  +offset));
 			for(int k=0 ; k< FRAME_WIDTH; k++){
-				printf("%c",left[i][j][k]);
+				printf("%c",movement[i][j][k]);
 			}
 			printf("\n");
 		}
@@ -34,30 +53,109 @@ for (l=1 ;l<3 ;l++){
 	
 	}
 }
+
+
+
+
+
+void fillFrames(File * fp ,action a){
+	int i,j,k;
+	for (i = 0; i< ACTION_LENGTH ;i++){
+		for(j = 0; j< FRAME_HEIGHT; j++){			
+			for(k=0 ;(c =fgetc(fp)) != '\n' && k< FRAME_WIDTH; k++){
+				a.frames[i][j][k] = c;
+			}
+				/*Complete rest of the line with ' ' */
+			if(k<FRAME_WIDTH){
+				while(k!=FRAME_WIDTH ){
+					a.frames[i][j][k++]=' ';
+				}
+			}
+			
+		}
+	}
 }
 
 
-int main(int argc, char const *argv[])
-{
-	int i,j,c;
+void openActions(char * fileRoute){
 	FILE *fp;
-	fp=fopen("lib.stickLib", "r");
+	fp = fopen(fileRoute, "r");	
+	while ( 1 ){
+		int i,j,k,c;
+		char buffer [200]={0};
+		i=0;
+		action a;
+		/*if((c =fgetc(fp)) == EOF){
+				break;
+		}else{
+			fputc(c,fp);
+		}*/
+		while ( (c =fgetc(fp)) != EOF ){
+			if( c == '[' ){
+				buffer[i]=0;
+				
+				if( isVariable(buffer) ){
+					a.name =malloc(strlen(buffer));
+					strcpy(a.name,buffer);
+					
+				} else {
+					printf("Not a valid name for library input: %s \n",buffer );
+					return ; 
+				}
+				i=0;
+				buffer[i++]=c;
+				while((c =fgetc(fp)) != '\n' ){
+					buffer[i++]=c;
+				}
+				buffer[i]=0;
+				if ( strcmp(buffer,"[right]" ) == 0){
+					a.direction = RIGHT;
+					break;
+				}else if ( strcmp(buffer,"[left]" ) == 0){
+					a.direction = LEFT;
+					break;
+				}else if ( strcmp(buffer,"[front]" ) == 0){
+					a.direction = FRONT;
+					break;
+				}else {
+					printf( "Not a valid attrbute for library input: %s\n",buffer);
+					return;
+				}
 
-	for (i=0; i<5 ;i++){
-		for(j=0; j<10; j++){
-			for(int k=0 ;(c =fgetc(fp)) != '\n' && k< 10; k++){
-				printf("%c",c );
-				left[i][j][k]=c;
+			}else {
+				buffer[i++]=c;
 			}
-			printf("\n");
 		}
-	
-	}
-		fclose(fp);
+		if ( c == EOF ){
+			printf("Not a valid stick library.\n");
+			return;
+		}
+		fillFrames(fp, a);
+		
 
-		for(i=5; i>0;i--){
-		printMovement(left,i);
+		
+		printf("Name: %s direction: %d \n",a.name,a.direction );
+		
+
+		
+		actions= realloc( actions, sizeof(action)*(actionsLen+1) );
+		memcpy( &(actions[actionsLen]) , &a , sizeof(action) );
+		actionsLen++;
+		
 	}
+
+	for (int i = 0; i < actionsLen; ++i)
+	{
+		printMovement( actions[i].frames,4 ,actions[i].direction);
+	}
+		
+	fclose(fp);
+
+}
+		
+
+int main(int argc, char const *argv[]){
+		openActions("lib.stickLib");
 	return 0;
 }
 
